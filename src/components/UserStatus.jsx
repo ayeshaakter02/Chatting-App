12dc1155ffe2993abc6ad9513f37efdc4ecddf7b
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2025-07-05 21:54:20 +0600

    Active status

--- src/components/UserStatus.jsx
+++ src/components/UserStatus.jsx
@@ -1,32 +1,32 @@
-// import { useEffect, useState } from "react";
-// import { ref, onValue, getDatabase } from "firebase/database";
+import { useEffect, useState } from "react";
+import { ref, onValue, getDatabase } from "firebase/database";
 
-// const UserStatus = ({ userId }) => {
+const UserStatus = ({ userId }) => {
 
-//     const db = getDatabase();
-//   const [status, setStatus] = useState("offline");
+    const db = getDatabase();
+  const [status, setStatus] = useState("offline");
 
-//   useEffect(() => {
-//     const statusRef = ref(db, `/status/${userId}`);
+  useEffect(() => {
+    const statusRef = ref(db, `/status/${userId}`);
 
-//     const unsubscribe = onValue(statusRef, (snapshot) => {
-//       if (snapshot.exists()) {
-//         setStatus(snapshot.val());
-//       }
-//     });
+    const unsubscribe = onValue(statusRef, (snapshot) => {
+      if (snapshot.exists()) {
+        setStatus(snapshot.val());
+      }
+    });
 
-//     return () => unsubscribe();
-//   }, [userId]);
+    return () => unsubscribe();
+  }, [userId]);
 
-//   return (
-//     <div>
-//       {status === "online" ? (
-//         <span style={{ color: "green" }}>● Active</span>
-//       ) : (
-//         <span style={{ color: "gray" }}>● Offline</span>
-//       )}
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      {status === "online" ? (
+        <span style={{ color: "green" }}>● Active</span>
+      ) : (
+        <span style={{ color: "gray" }}>● Offline</span>
+      )}
+    </div>
+  );
+};
 
-// export default UserStatus;
+export default UserStatus;
