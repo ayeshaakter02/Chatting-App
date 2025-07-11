4e9dfcda031cdea6402478ca834cfc68fcb3ff11
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2025-07-11 19:54:23 +0600

    res Home page

--- src/components/FriendRequestList.jsx
+++ src/components/FriendRequestList.jsx
@@ -39,7 +39,7 @@ const FriendRequestList = () => {
     <>
       {/* component */}
       {/* This is an example component */}
-      <div className="md:ml-30 xl:ml-0 md:w-150 xl:w-auto md:mt-20 xl:mt-0">
+      <div className="sm:ml-15 md:ml-30 xl:ml-0 md:w-150 xl:w-auto md:mt-20 xl:mt-0">
         <div className="max-w-lg rounded-lg border p-4 shadow-md sm:p-8 dark:border-gray-700 dark:bg-gray-800 backdrop-blur-xl">
           <div className="mb-4 flex items-center justify-between">
             <h3 className="text-xl leading-none font-bold text-white dark:text-white">
